7f0b411d4dccecfd4cdca8501527fb86480a04ab
MiracleWong <cfwr1991@126.com>
2017-06-07 15:46:15 +0800

    show the error messages

--- sample_app/app/helpers/users_helper.rb
+++ sample_app/app/helpers/users_helper.rb
@@ -1,8 +1,7 @@
 module UsersHelper
   # 返回指定用户的gravatar
-  def gravatar_for(user, options = { size: 80})
+  def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    siez = options[:size]
     gravatar_url = "https://s.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar") 
   end
