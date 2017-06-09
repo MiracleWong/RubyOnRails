ef68ba9b894249e17caa400c93f0e19aafb9adbe
MiracleWong <cfwr1991@126.com>
2017-06-09 12:23:13 +0800

    Add the remember me

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -10,4 +10,26 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # 返回一个随机令牌
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 如果指定的令牌和摘要匹配，返回true
+  def authenticated?(remember_token)
+    ## 在记忆摘要的值为nil的时候，authenticated?返回值为false
+    return false if remember_token.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  ## 忘记用户
+  def forget
+    update_attribute(:remember_digest, nil)
+  end 
 end
--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608063319) do
-
-  create_table "students", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170609030122) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -26,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170608063319) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.string "remember_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
