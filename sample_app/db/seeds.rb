b5f9be7a9f3a333adc876ed23bc32ebad083ec53
MiracleWong <cfwr1991@126.com>
2017-06-14 18:46:11 +0800

    add the seed

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -36,11 +36,11 @@ users = User.order(:created_at).take(6)
 end
 
 
-# # Following relationships
-# users = User.all
-# user = users.first
+# Following relationships
+users = User.all
+user = users.first
 
-# following = users[2..50]
-# followers = users[3..40]
-# following.each { |followed| user.follow(followed) }
-# followers.each { |follower| follower.follow(user) }
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
